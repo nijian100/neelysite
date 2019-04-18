e1e8ef276252bcfdb66621bb9a1981f41a2ac2f5
Unknown <nj17671@ly.com>
2019-04-18 10:57:23 +0800

    4.18、向 polls/views.py 里添加更多视图
    4.18、向 polls/views.py 里添加更多视图

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello,world.You're at the polls index.")
+
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request,qquestion_id):
+    response = "You're looking ay the result %s."
+    return HttpResponse(response % qquestion_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
